577b260f84a8134e26245e07f1988f9a96555857
Kerem Çelik <93091784+antcel07@users.noreply.github.com>
2023-01-20 18:56:31 +0300

    Add files via upload

---
+++ ifElse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ifElse
+{
+ class Program
+ {
+    static void Main(string[] args)
+    {
+        int time=DateTime.Now.Hour;
+        if(time<=10)
+        {
+            Console.WriteLine("Günaydin");
+        }
+        else if(time>10 && time<18)
+        {
+            Console.WriteLine("İyi Günler");
+        }
+        else{
+            Console.WriteLine("İyi Akşamlar");
+        }
+
+
+        string sonuc=time<18 ? "İyi günler" : "İyi aksamlar";
+sonuc=time>6 && time<11 ? "Günaydin" : time<18 ? "İyi günler" : "İyi aksamlar";
+Console.WriteLine(sonuc);
+
+    }
+ }  
+}
+
