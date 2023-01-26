cf3774c562f12935b8dcdd1f8634df86a682235d
Kerem Çelik <93091784+antcel07@users.noreply.github.com>
2023-01-26 14:09:18 +0300

    Add files via upload

---
+++ abstractOrnek/Otomobil.cs
@@ -0,0 +1,13 @@
+namespace abstracOrnek{
+
+public abstract class Otomobil{
+    public int kacTekerlektenOlusur(){
+        return 4;
+    }
+
+    public virtual Renk standartRengiNe(){
+   
+    }
+}
+
+}
